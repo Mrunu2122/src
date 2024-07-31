3e7f5e8c4f73fa73897c65bede456321a94ce81a
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-07-31 16:18:49 +0530

    react

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./components/Nav"
+import HomePage from "./components/HomePage"
+import ProductSection from "./components/products"
+
+const App =() => {
+
+  return (
+    <div className="App">
+      
+      <NavBar />
+      <HomePage />
+      <ProductSection />
+    </div>
+    
+  )
+}
+
+export default App
